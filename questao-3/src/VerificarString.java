14641a993785211d46b489396c84e39225175645
pink_glb <adriancosta1215@gmail.com>
2022-06-22 18:33:58 -0400

    questao 3 refatorada

--- questao-3/src/VerificarString.java
+++ questao-3/src/VerificarString.java
@@ -22,36 +22,37 @@ public class VerificarString {
 				System.out.println("Digite sua frase");
 				frase = scan.nextLine();
 			}
-
+			
+			// verifica quantos "emojis" tem na frase e o contador itera de acordo com o humor do emoji, "a" para divertido
+			// e "b" para chateado
+			
 			for (int i = 0; i < frase.length(); i++) {
-				try {
-					if (frase.charAt(i) == ':' && frase.charAt(i + 1) == '-' && frase.charAt(i + 2) == ')'
-							&& frase.charAt(i + 3) != '~') {
-						contador += "a";
-					}
-					if (frase.charAt(i) == ':' && frase.charAt(i + 1) == '-' && frase.charAt(i + 2) == '('
-							&& frase.charAt(i + 3) != '~') {
-						contador += "b";
-					}
-				} catch (StringIndexOutOfBoundsException e) {
-					if (frase.length() - 1 == frase.lastIndexOf(')')) {
-						contador += "a";
-					} else if (frase.length() - 1 == frase.lastIndexOf('(')) {
-						contador += "b";
-					}
-				}
-			}
 
+				if (frase.charAt(i) == ':' && frase.charAt(i + 1) == '-' && frase.charAt(i + 2) == ')') {
+					contador += "a";
+				}
+
+				if (frase.charAt(i) == ':' && frase.charAt(i + 1) == '-' && frase.charAt(i + 2) == '(') {
+					contador += "b";
+				}
+
+			}
+			
+			//como o indicado é que uma frase só tenha o emoji de só um tipo de humor, então o teste de cada humor verifica se só 
+			//um tipo de humor na frase, caso sim a frase recebe aquele humor, caso contrário recebe "neutro" 
+			
 			for (int c = 0; c < contador.length(); c++) {
 				testeDivertido += "a";
 				testeChateado += "b";
 			}
-
+			
+			//caso não haja nenhum emoji na frase ele recebe um falor default para ser diferenciada do contador
+			
 			if (testeDivertido.equals("")) {
-				testeDivertido = "1";
+				testeDivertido = "default";
 			}
 			if (testeChateado.equals("")) {
-				testeChateado = "1";
+				testeChateado = "default";
 			}
 
 			if (contador.equals(testeDivertido)) {
@@ -61,15 +62,15 @@ public class VerificarString {
 			} else {
 				humor = "neutro";
 			}
+			
 			System.out.println(humor + " \n");
 			System.out.println("Deseja verificar o humor de outra frase?");
 			System.out.println("1 - Para finalizar");
 			System.out.println("2 - Para executar novamente");
 			int resp = new Scanner(System.in).nextInt();
 			if (resp == 1) {
-				break;
+				running = false;
 			}
-
 		}
 	}
 }
